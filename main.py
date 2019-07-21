1ef1fed71ff805f27f1b7e051a19e20a0aaec31a
Wayne <djs860403@gmail.com>
2019-07-21 21:20:39 +0800

    Init logging

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import pdb
+import logging
+
+logging.basicConfig(    level = logging.DEBUG,  
+                                             format='%(asctime)s - %(levelname)s : %(message)s',  
+                                             filename="log.txt'")
+
+'''==========================================================
+                    NOSET < DEBUG < INFO < WARNING < ERROR < CRITICAL
+                    >>> logging.critical("critical message")  
+                    >>> logging.debug("debug message")  
+                    >>> logging.info("info message")  
+                    >>> logging.warning("warning message")  
+                    >>> logging.error("error message")  
+                    >>> logging.critical("critical message")  
+=========================================================='''
+def main():
+                        
+    print ("AliAs")
+    
+if  __name__ == "__main__":
+    main()
