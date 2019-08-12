5062191f7b9ad952307c945478bdc6eece270a22
Wayne <djs860403@gmail.com>
2019-08-12 22:15:50 +0800

    Setting layout
    1. Using bootstrap format to assign layouy
    2. script is imported by static folder

---
+++ AliAs/AliAs/main/static/main/scripts/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$("#search_btn").click(function(){
+		var search_msg = $("#search_target").val();
+		$.get("/search/", {'search_msg' : search_msg}, function(ret){
+			$('#msgcontent').html(ret);
+		})
+	});
+
+	$("#imgclick").click(function(){
+		var search_msg = $("#search_target").val();
+		$.get("/search/", {'search_msg' : search_msg}, function(ret){
+			$('#msgcontent').html(ret);
+		})
+	});
+
+	$('#circle').click(function(){
+		$(this).animate({width:'50px',
+						height:'50px'});
+	});
+	
+	$("#loopBtn").click(function(){
+		setInterval(function(){
+			$("#img").animate({	
+								width:"+=1px"});
+			
+			console.log($('#img').width());
+		}, 500);
+	});
+});
