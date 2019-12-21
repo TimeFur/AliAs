a950c370631e2eb84e9fd1a0620e7bc2ee73d6de
Wayne <djs860403@gmail.com>
2019-12-21 15:10:04 +0800

    Revise screen shot js name
    1. Remove source code in html
    2. Rename screenshot js

--- AliAs/AliAs/main/static/main/scripts/screenshot.js
+++ AliAs/AliAs/main/static/main/scripts/screenshot.js
@@ -5,19 +5,28 @@ async function screenShot(){
 		text: "Screen Shot!" 
 	};
 	
-	//send to extension
+	console.log("Scrrenshot");
+	//send to extension [content]
 	window.postMessage(data, "*");
 	
-	//wait response from extension
+	//wait response from extension [content]
 	window.addEventListener('message', function(event){
+		var imgSrc = document.getElementById("img_id");
 		
 		if (event.source != window) 
 			return;
 
 		if (event.data.type && event.data.type == 'FROM_EXTENSION') {
-			console.log('Content script received: ' + event.data.text);
+			console.log('From Content script received: ' + event.data.imgSrc);
+			
+			//set to image src	
+			imgSrc.src = event.data.imgSrc;
+			
+			//
 		}
 	  },
 	  false
 	);
+	
+	return true;
 }
