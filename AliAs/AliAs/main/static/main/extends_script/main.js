de903fff4ada51e528ca18ee0db3064807c56389
Wayne <djs860403@gmail.com>
2019-12-23 22:14:09 +0800

    Extension open url
    1. Check the current url and open alias website
    2. Setting height for left-side col

--- AliAs/AliAs/main/static/main/extends_script/main.js
+++ AliAs/AliAs/main/static/main/extends_script/main.js
@@ -1,9 +1,27 @@
 document.getElementById("scrshot_id").addEventListener("click", function(){
 	
-	document.getElementById("response_id").innerHTML="Get click!";
-	
 	//https://stackoverflow.com/questions/4573956/taking-screenshot-using-javascript-for-chrome-extensions/4574782
 	chrome.tabs.captureVisibleTab(null, {}, function (image_url) {
 		document.getElementById("response_id").innerHTML=image_url;
 	});
+});
+
+chrome.tabs.query({"currentWindow":true, 'active': true}, function(tabs){
+	var url = tabs[0].url;
+	var id = tabs[0].id;
+	var regex = 'https://www.youtube.com/watch\\?v='
+	var createUrl = "http://127.0.0.1:8000/alias/";
+	
+	// var executeCommand = 'var data = { type: "FROM_PAGE", text: "Screen Shot!" };	window.postMessage(data, "*");';
+	var executeCommand = 'console.log("Get in")';
+	
+	
+	console.log("Current URL = " + url);
+	if (url.match(regex))
+	{
+		//open alias website
+		chrome.tabs.create({'url': createUrl}, function(tabs){
+			
+		});	
+	}
 });
