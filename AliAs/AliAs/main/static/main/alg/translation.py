40ce03cd1f2d89dbe16ae3ab3e6e24e37349f184
Wayne <djs860403@gmail.com>
2019-12-31 00:17:46 +0800

    Using googletrans module to translation
    1. Import googletrans to translate
    [https://py-googletrans.readthedocs.io/en/latest/]

---
+++ AliAs/AliAs/main/static/main/alg/translation.py
@@ -0,0 +1,13 @@
+import os
+from googletrans import Translator
+
+def translationFunc(text):
+    trans = Translator().translate(text, dest = "zh-tw")
+    return (trans.text)
+    
+def main():
+    result = translationFunc('Wayne')
+    print (result)
+    
+if __name__ == "__main__":
+    main()
