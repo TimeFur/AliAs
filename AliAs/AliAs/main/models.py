ff5e62a3b6df0c861bae856f4ee10a96e91a6add
Wayne <djs860403@gmail.com>
2020-01-17 00:04:22 +0800

    DB models for admin
    1. Setting structure of PostImgList models.
    2. Let models registered by admin

--- AliAs/AliAs/main/models.py
+++ AliAs/AliAs/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class PostImgList(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField(blank=True)
+    curtime = models.CharField(max_length = 100)
+    imgsrc = models.ImageField(upload_to = 'photo/') #subdirectory of MEDIA_ROOT to use for uploaded files
